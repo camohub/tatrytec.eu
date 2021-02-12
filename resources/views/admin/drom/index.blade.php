438f6b1e3c99a8a339e1619f914e08c51c6b32ef
Čamaj Vladimír <vladimir.camaj@gmail.com>
2021-02-12 11:00:55 +0100

    Admin drom/index.blade.php css fix

--- resources/views/admin/drom/index.blade.php
+++ resources/views/admin/drom/index.blade.php
@@ -4,8 +4,8 @@
 
 <div class="row">
 
-	<h1 class="col-xs-12 col-sm-5">Drom</h1>
-	<div class="col-12 col-sm-5">Huraaaaaaaaaa email bol odoslaný alebo sa tak aspoň tvári.</div>
+	<h1 class="col-12">Drom</h1>
+	<div class="col-12">Huraaaaaaaaaa email bol odoslaný alebo sa tak aspoň tvári.</div>
 </div>
 
 @endsection
