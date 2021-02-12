221eed234d36de64369de37dd43a53ff1699b90a
Čamaj Vladimír <vladimir.camaj@gmail.com>
2021-02-12 10:56:58 +0100

    Admin DromController.php

--- app/Http/Controllers/Admin/DromController.php
+++ app/Http/Controllers/Admin/DromController.php
@@ -4,13 +4,16 @@ namespace App\Http\Controllers\Admin;
 
 
 use App\Http\Controllers\BaseController;
+use App\Mail\Test;
+use Illuminate\Support\Facades\Mail;
 
 
-class DefaultController extends BaseController
+class DromController extends BaseController
 {
 
 	public function index()
 	{
-		return view('admin.index');
+		Mail::to('vladimir.camaj@gmail.com')->send(new Test());
+		return view('admin.drom.index');
 	}
 }
---
+++ resources/views/admin/drom/index.blade.php
@@ -0,0 +1,11 @@
+@extends('layout-admin')
+
+@section('content')
+
+<div class="row">
+
+	<h1 class="col-xs-12 col-sm-5">Drom</h1>
+	<div class="col-12 col-sm-5">Huraaaaaaaaaa email bol odoslaný alebo sa tak aspoň tvári.</div>
+</div>
+
+@endsection
