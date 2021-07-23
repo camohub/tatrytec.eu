e8a0d5f350d6140d97eaefa5f891a3880beb54fb
Čamaj Vladimír <vladimir.camaj@gmail.com>
2021-07-23 12:16:29 +0200

    SlimArticlesResource.php user fix

--- app/Http/Resources/SlimArticleResource.php
+++ app/Http/Resources/SlimArticleResource.php
@@ -17,7 +17,7 @@ class SlimArticleResource extends JsonResource
 		return [
 			'id' => $this->id,
 			'title' => $this->title,
-			'user' => ['id' => $this->user->id, 'email' => $this->user->email],
+			'user' => ['id' => $this->user->id, 'name' => $this->user->name, 'email' => $this->user->email],
 			'visible' => $this->visible,
 			'created_at' => $this->created_at->getTimestamp(),
 		];
